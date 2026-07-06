59bf8dd2b6941b95845b9c82bd87795124173f2a
reviskor <revissskor@gmail.com>
2026-07-06 15:18:46 +0300

    add comments to task 5

--- DAY_1/Task5.c
+++ DAY_1/Task5.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
 int main(void) {
-    int x = 15, y = 20, z = x & y, k = x | y, l = x ^ y, m = x>>3, n = y << 4;
+    int x = 15,
+    y = 20, 
+    z = x & y, // BTIWISE AND
+    k = x | y, // BITWISE OR
+    l = x ^ y, // BITWISE XOR
+    m = x>>3, // SHIFT RIGHT BY 3
+    n = y << 4; // SHIFT LEFT BY 4
 
     printf("%d is %b\n", z, z);
     printf("%d is %b\n", k, k);
